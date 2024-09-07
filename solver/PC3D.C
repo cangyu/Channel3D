2545b4fe1921420c7c0819b8eaa6de51e9d0063e
Yu Cang <yhcy1993@gmail.com>
2024-09-07 15:51:41 +0800

    cell identification done

--- solver/PC3D.C
+++ solver/PC3D.C
@@ -1,4 +1,21 @@
-#include "fvCFD.H"
+#include "argList.H"
+#include "Time.H"
+#include "fvMesh.H"
+#include "fvc.H"
+#include "fvm.H"
+#include "timeSelector.H"
+#include "labelIOField.H"
+#include "labelFieldIOField.H"
+#include "labelList.H"
+#include "scalarIOField.H"
+#include "scalarFieldIOField.H"
+#include "vectorIOField.H"
+#include "vectorFieldIOField.H"
+#include "vectorList.H"
+#include "tensorIOField.H"
+#include "tensorFieldIOField.H"
+#include "tensorList.H"
+#include "pointFields.H"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -7,6 +24,11 @@
 #include <numeric>
 #include <limits>
 #include <string>
+
+bool isEqual(double x, double y)
+{
+  return std::abs(x-y) <= 1e-6 * std::abs(x);
+}
 
 /* Constants */
 const Foam::scalar s2ns = 1e9, ns2s = 1.0/s2ns;
@@ -27,6 +49,11 @@ const Foam::scalar Pr = 1.0;    // Prandtl number
 const Foam::scalar Le = 1.0;    // Lewis number
 const Foam::scalar Re = 100.0;  // Reynolds number
 
+/* Classification of gas-phase cells */
+const Foam::scalar cFluid = 1.0;
+const Foam::scalar cIB = 0.0;
+const Foam::scalar cSolid = -1.0;
+
 void update_boundaryField(const Foam::fvMesh &mesh, const Foam::volScalarField &src1, const Foam::volVectorField &src2, Foam::surfaceScalarField &dst)
 {
 	for (int pI = 0; pI < mesh.boundary().size(); pI++)
@@ -38,6 +65,38 @@ void update_boundaryField(const Foam::fvMesh &mesh, const Foam::volScalarField &
 		auto &patch_dst = dst.boundaryFieldRef()[pI];
 		for (int fI = 0; fI < curPatch.size(); fI++)
 			patch_dst[fI] = patch_val1[fI] * patch_val2[fI] & patch_Sn[fI];
+	}
+}
+
+bool pnt_inSolid(const Foam::vector &c)
+{
+	const Foam::vector c0(0.5, 0.5, 0.5);
+	const Foam::scalar r = 0.15;
+
+	return Foam::mag(c-c0) < r;
+}
+
+bool cell_isIntersected(const Foam::vectorList &v)
+{
+	bool flag = pnt_inSolid(v[0]);
+	for (int i = 1; i < v.size(); i++)
+	{
+		if (pnt_inSolid(v[i]) != flag)
+			return true;
+	}
+	return false;
+}
+
+Foam::scalar cellNum(const Foam::vectorList &v, const Foam::vector &c)
+{
+	if (pnt_inSolid(c))
+		return cSolid;
+	else
+	{
+		if (cell_isIntersected(v))
+			return cIB;
+		else
+			return cFluid;
 	}
 }
 
@@ -56,11 +115,61 @@ int main(int argc, char *argv[])
 	rhoUSn = Foam::fvc::interpolate(rhoU) & mesh_gas.Sf();
 	update_boundaryField(mesh_gas, rho, U, rhoUSn);
 
+	/* Classify mesh cells */
+	{
+		for (int i = 0; i < mesh_gas.nCells(); i++)
+		{
+			const auto &c = mesh_gas.C()[i];
+
+			const auto &cP = mesh_gas.cellPoints()[i];
+			Foam::vectorList p(cP.size());
+			for (int j = 0; j < p.size(); j++)
+				p[j] = mesh_gas.points()[cP[j]];
+
+			cMarker[i] = cellNum(p, c);
+		}
+
+		for (int i = 0; i < mesh_gas.nCells(); i++)
+		{
+			if (isEqual(cMarker[i], cSolid))
+			{
+				const auto &cC = mesh_gas.cellCells()[i];
+				for (int j = 0; j < cC.size(); j++)
+				{
+					const auto adjCI = cC[j];
+					if (isEqual(cMarker[adjCI], cFluid))
+						cMarker[adjCI] = cIB;
+				}
+			}
+		}
+
+		for (int i = 0; i < mesh_gas.nCells(); i++)
+		{
+			if (cMarker[i] > 0)
+				cIbMask[i] = 1.0;
+			else
+				cIbMask[i] = 0.0;
+		}
+	}
+
 	while(runTime.loop())
 	{
 		const Foam::dimensionedScalar dt(Foam::dimTime, runTime.deltaTValue());
 		Foam::Info << "\nn=" << runTime.timeIndex() << ", t=" << std::stod(runTime.timeName(), nullptr)*s2ms << "ms, dt=" << dt.value()*s2ns << "ns" << Foam::endl;
 		runTime.write();
+
+		/* Interpolation on IB cells */
+		{
+			for (int i = 0; i < mesh_gas.nCells(); i++)
+			{
+				if (isEqual(cMarker[i], cIB))
+				{
+					U[i].x() = 0.0;
+					U[i].y() = 0.0;
+					U[i].z() = 0.0;
+				}
+			}
+		}
 
 		/* Update gas-phase @(n) with Tw @(n) as Dirichlet B.C. and sources terms @(n) */
 		{
@@ -71,16 +180,6 @@ int main(int argc, char *argv[])
 			{
 				Foam::Info << "m=" << m << Foam::endl;
 
-				/* Update the position of immersed boundary */
-				const Foam::vector c0(0.5, 0.5, 0.5);
-				const Foam::scalar r = 0.1;
-				for (int i = 0; i < mesh_gas.nCells(); i++)
-				{
-					const auto &c = mesh_gas.C()[i];
-					if (Foam::mag(c-c0) < r)
-						cMask[i] = 0.0;
-				}
-
 				/* Predictor */
 				{
 					/* Provisional momentum */
@@ -89,12 +188,10 @@ int main(int argc, char *argv[])
 						U_star = U;
 						Foam::fvVectorMatrix UEqn
 						(
-							Foam::fvm::ddt(rho, U_star) + Foam::fvc::div(rhoUSn, U_next) == -grad_p + 0.5*(Foam::fvm::laplacian(mu, U_star)+Foam::fvc::laplacian(mu, U_next))
+							Foam::fvm::ddt(rho, U_star) + Foam::fvc::div(rhoUSn, U_next) == -cIbMask * grad_p + 0.5*(Foam::fvm::laplacian(mu, U_star)+Foam::fvc::laplacian(mu, U_next))
 						);
 						UEqn.solve();
 					}
-
-					U_star *= cMask;
 
 					/* Provisional mass flux */
 					rhoU_star = rho * U_star;
@@ -112,6 +209,11 @@ int main(int argc, char *argv[])
 					(
 						dt * Foam::fvm::laplacian(dp) == Foam::fvc::div(rhoUSn_star)
 					);
+					for (int i=0; i < mesh_gas.nCells(); i++)
+					{
+						if (cMarker[i] < cFluid)
+							dpEqn.source()[i] = 0.0;
+					}
 					dpEqn.solve();
 
 					/* Update */
