17dd1a9d906fdb8eb40ffb250f68ece6f0b2d029
Yu Cang <yu.cang@sjtu.edu.cn>
2024-11-24 18:06:48 +0800

    adapt to the inplace version of the flow solver

---
+++ incompressible/dpEqn_Semi-Implicit_GCIBM_Inplace/case/auto_clean.sh
@@ -0,0 +1,16 @@
+wclean ../solver
+rm -rf ../solver/pc3d
+rm -rf ../solver/PC3D
+rm -rf processor*
+rm -rf ifc_p*
+rm -rf msf_p*
+rm -rf *e-10
+rm -rf pnt_af_*
+rm -rf pnt_bf_*
+rm -rf face_af_*
+rm -rf face_bf_*
+rm -rf cell_*
+rm -rf list_cmp.txt
+rm -rf *cpu*
+rm -rf constant/*/polyMesh
+rm -rf constant/polyMesh
